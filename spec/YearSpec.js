481c0936e6f73bbb7018bc902f8a5c8a454c38ec
Hung Ka Kin <hungkakin@Hungs-MacBook-Pro.local>
2018-09-30 23:11:48 +0800

    Add test not divisible by 400

--- spec/YearSpec.js
+++ spec/YearSpec.js
@@ -2,6 +2,11 @@ describe("LeapYear", () => {
   // var leapyear;
 
   it('returns leapyear when year is divisible by 400', () => {
-    expect(LeapYear(400)).toEqual('Year 400 is a leap year')
+    expect(LeapYear(2000)).toEqual('Year 2000 is a leap year')
   });
+
+  it('returns year when year divisible by 100 by not divisible 400', () => {
+    expect(LeapYear(1700)).toEqual('Year 1700 is not a leap year')
+  });
+  
 });
--- src/Year.js
+++ src/Year.js
@@ -1,3 +1,15 @@
 function LeapYear(year) {
-  return 'Year ' + year + ' is a leap year'
+  if (isDivisibleBy400(year) && isDivisibleBy100(year)) {
+    return 'Year ' + year + ' is a leap year' }
+  else { 
+    return 'Year ' + year + ' is not a leap year'
+  }
+}
+
+function isDivisibleBy400(year) {
+  return year % 400 === 0
+}
+
+function isDivisibleBy100(year) {
+  return year % 100 === 0
 }
