c54ad45c782a0a6ae178f28f39a209bb17dbac52
Hung Ka Kin <hungkakin@Hungs-MacBook-Pro.local>
2018-09-30 23:55:49 +0800

    Add returns leap year when divisible by 4 not by 100

--- spec/YearSpec.js
+++ spec/YearSpec.js
@@ -8,5 +8,12 @@ describe("LeapYear", () => {
   it('returns year when year divisible by 100 by not divisible 400', () => {
     expect(LeapYear(1700)).toEqual('Year 1700 is not a leap year')
   });
-  
+
+  it('returns leap year when year divisible by 4 by not divisible 100', () => {
+    expect(LeapYear(2008)).toEqual('Year 2008 is a leap year')
+  });
+
+  it('returns leap year when year not divisible by 4', () => {
+    expect(LeapYear(2017)).toEqual('Year 2017 is not a leap year')
+  });
 });
--- src/Year.js
+++ src/Year.js
@@ -1,9 +1,23 @@
+// function LeapYear(year) {
+//   if (isDivisibleBy100(year)) {
+//     if(isDivisibleBy400(year)) {
+//       return 'Year ' + year + ' is a leap year' }
+//     else {
+//       return 'Year ' + year + ' is not a leap year' }
+//     }
+//   else if (isDivisibleBy4(year)) {
+//     return 'Year ' + year + ' is a leap year'
+//   } else {
+//     return 'Year ' + year + ' is not a leap year'
+//   }
+// }
+
+
 function LeapYear(year) {
-  if (isDivisibleBy400(year) && isDivisibleBy100(year)) {
-    return 'Year ' + year + ' is a leap year' }
-  else { 
-    return 'Year ' + year + ' is not a leap year'
-  }
+  if (isDivisibleBy100(year) && isDivisibleBy400(year) || isDivisibleBy4(year) && !isDivisibleBy100(year)) {
+    return 'Year ' + year + ' is a leap year';
+  } else {
+  return 'Year ' + year + ' is not a leap year'; }
 }
 
 function isDivisibleBy400(year) {
@@ -12,4 +26,8 @@ function isDivisibleBy400(year) {
 
 function isDivisibleBy100(year) {
   return year % 100 === 0
+}
+
+function isDivisibleBy4(year) {
+  return year % 4 === 0
 }
